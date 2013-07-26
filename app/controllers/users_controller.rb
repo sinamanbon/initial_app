e1354dcbda35047d5e22fd3213a465b7a3c66d86
sinamanbons <sinamanbon@gmail.com>
2013-07-26 11:29:23 -0400

    appending flash to application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) #extremely dangerous - throws error when working with Rails 4.0
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
