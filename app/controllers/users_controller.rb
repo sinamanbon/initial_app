51343bcc86aa08beab7d204bc27be76f9f26412a
sinamanbons <sinamanbon@gmail.com>
2013-07-26 11:14:56 -0400

    added errors to signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params) #extremely dangerous - throws error when working with Rails 4.0
+    if @user.save
+      #successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+  # new way to permit only listed attributes for user params
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
