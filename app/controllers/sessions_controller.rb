1e14bc28c8195a96317a7c742c76ef2a3279e1bd
Joseph Lee <sinamanbon@gmail.com>
2013-07-27 02:08:55 -0400

    created sign up page that throws flash.now error if email/password is invalid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    #user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #sign the user in and redirect to the user's show page
+    else
+      #creates and error message and re-renders the signin form.
+      #user flash.now to make contents dissapear as soon as there is another request
+      flash.now[:error] = 'Invalid email/password combination'
+      render "new"
+    end
 
   end
 
