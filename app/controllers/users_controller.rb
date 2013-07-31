c15873bf1fe5590b065805dd42428eed0b782d4d
sinamanbons <sinamanbon@gmail.com>
2013-07-31 10:35:09 -0400

    added correct_user that sets @user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only:[:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,11 +22,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in(@user)
@@ -44,6 +44,12 @@ class UsersController < ApplicationController
   # Before filters
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    #contains the information about the user initially
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
