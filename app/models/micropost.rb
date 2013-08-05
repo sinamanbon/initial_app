221a1ec2b4ec558d50db6adaf05d59f821e96cde
sinamanbons <sinamanbon@gmail.com>
2013-08-05 13:52:28 -0400

    validates presence of content for microposts with a maximum length of 140

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,5 +4,8 @@ class Micropost < ActiveRecord::Base
   #ordering by descending
   default_scope -> { order ('created_at DESC') }
 
+  #validates the content row for presence and setting maximum length
+  validates :content, presence:true, length: { maximum: 140}
+
   validates :user_id, presence: true
 end
