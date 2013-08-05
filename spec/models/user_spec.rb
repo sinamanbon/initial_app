80e380e78d6485fad100f54983660ef164dee596
sinamanbons <sinamanbon@gmail.com>
2013-08-04 20:11:04 -0400

    testing the order of a user's microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,21 @@ describe User do
     it { should be_admin}
   end
 
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
+
   describe "remember token" do
     before {@user.save}
     its(:remember_token) { should_not be_blank }
