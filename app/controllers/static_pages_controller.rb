30aa8510b539e10367f3696edbb83c36fecc6c56
sinamanbons <sinamanbon@gmail.com>
2013-08-09 11:53:06 -0400

    added to home only if user has signed in to show posts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if signed_in?
     # defining @micropost instance variable for usage for home page
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+      @micropost = current_user.microposts.build if signed_in?
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
