2e0ac65e7499bf25a79c6046c4dab4bdee2e413c
sinamanbons <sinamanbon@gmail.com>
2013-08-13 11:05:58 -0400

    added the method following and followers in parallel to routes.rb's users member

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
   # new way to permit only listed attributes for user params
   def user_params
